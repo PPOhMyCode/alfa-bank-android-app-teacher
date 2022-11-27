816849c38af4d05840fabf4c17419b0193e329a3
DanilBychkov <Danil.Bychkov@urfu.me>
2022-11-27 10:48:05 +0500

    Create new project

---
+++ app/src/main/java/com/example/alfa_bank_android_app_teacher/ui/gallery/GalleryViewModel.kt
@@ -0,0 +1,13 @@
+package com.example.alfa_bank_android_app_teacher.ui.gallery
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class GalleryViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is gallery Fragment"
+    }
+    val text: LiveData<String> = _text
+}
